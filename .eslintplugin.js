aec34b800dc363b893cd4a0f537dcc8cc715e417
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-11-26 00:08:50 +0100

    support autofix for translators

--- .eslintplugin.js
+++ .eslintplugin.js
@@ -1,13 +1,30 @@
+function fixMessage(m, offset) {
+  if (offset) {
+    m.line += offset.lines;
+    if (typeof m.endLine === 'number') m.endLine += offset.lines;
+
+    if (m.fix) m.fix.range = m.fix.range.map(c => c + offset.chars);
+  }
+
+  return m;
+}
+
 module.exports = {
   processors: {
     // assign to the file extension you want (.js, .jsx, .html, etc.)
     ".js": {
         // takes text of the file and filename
         preprocess: function(text, filename) {
+            if (!this.offset) this.offset = {}
+
             if (filename.indexOf('/resource/translators/') < 0) return [text];
 
-            const translator = text.match(/^([\s\S]+?})(\n\n[\s\S]+)/);
-            return [`const zotero_translator_header = ${translator[1]};${translator[2]}`];
+            const translator = text.match(/^([\s\S]+?\r?\n}(\r?\n)+)([\s\S]+)/);
+            this.offset[filename] = {
+              lines: translator[1].split('\n').length - 1,
+              chars: translator[1].length,
+            }
+            return [translator[3]];
         },
 
         // takes a Message[][] and filename
@@ -17,10 +34,10 @@ module.exports = {
             // to the text that was returned in array from preprocess() method
 
             // you need to return a one-dimensional array of the messages you want to keep
-            return [].concat.apply([], messages);
+            return messages[0].map(m => fixMessage(m, this.offset[filename]));
         },
 
-        supportsAutofix: false
+        supportsAutofix: true
     }
   }
 };
