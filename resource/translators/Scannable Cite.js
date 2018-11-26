01d0c91e8ac9b53bf3dca11abdfb5b8668f0e6e6
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2018-11-26 12:16:53 +0100

    cleanup with less changes

--- resource/translators/Scannable Cite.js
+++ resource/translators/Scannable Cite.js
@@ -1,53 +1,37 @@
 {
-    "translatorID": "248bebf1-46ab-4067-9f93-ec3d2960d0cd",
-    "label": "Scannable Cite",
-    "creator": "Scott Campbell, Avram Lyon, Nathan Schneider, Sebastian Karcher, Frank Bennett",
-    "target": "html",
-    "minVersion": "3.0",
-    "maxVersion": "",
-    "priority": 100,
-    "displayOptions": {
-        "exportCharset": "UTF-8"
-    },
-    "inRepository": true,
-    "translatorType": 2,
-    "browserSupport": "",
-    "lastUpdated": "2013-04-16 11:10:08"
+	"translatorID": "248bebf1-46ab-4067-9f93-ec3d2960d0cd",
+	"label": "Scannable Cite",
+	"creator": "Scott Campbell, Avram Lyon, Nathan Schneider, Sebastian Karcher, Frank Bennett",
+	"target": "html",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 100,
+	"displayOptions": {
+		"exportCharset": "UTF-8"
+	},
+	"inRepository": true,
+	"translatorType": 2,
+	"browserSupport": "",
+	"lastUpdated": "2013-04-16 11:10:08"
 }
 
 // legal types are weird
 const LEGAL_TYPES = ["bill","case","gazette","hearing","patent","regulation","statute","treaty"];
 const Mem = function (item) {
-    const lst = [];
+    let lst = [];
     const isLegal = (LEGAL_TYPES.indexOf(item.itemType)>-1);
-
-    this.set = function (str, punc, slug) {
-        if (!punc) punc="";
-
-        if (str) {
-            lst.push((str + punc));
-        } else if (!isLegal) {
-            lst.push(slug);
-        }
-    };
-
-    this.setlaw = function (str, punc) {
-        if (!punc) punc="";
-        if (str && isLegal) lst.push(str + punc);
-    };
-
-    this.get = function () {
-        return lst.join(" ");
-    };
+    this.set = function (str, punc, slug) { if (!punc) {punc="";} if (str) {lst.push((str + punc));} else if (!isLegal) {lst.push(slug);}};
+    this.setlaw = function (str, punc) { if (!punc) {punc="";} if (str && isLegal) {lst.push(str + punc);}};
+    this.get = function () { return lst.join(" "); };
 };
 
 function doExport() {
     let item;
     while (item = Zotero.nextItem()) {
-        const mem = new Mem(item);
-        const memdate = new Mem(item);
+        let mem = new Mem(item);
+        let memdate = new Mem(item);
         Zotero.write("{ |");
-        const library_id = item.libraryID ? item.libraryID : 0;
+        let library_id = item.libraryID ? item.libraryID : 0;
         if (item.creators.length >0){
             mem.set(item.creators[0].lastName,",");
             if (item.creators.length > 2) mem.set("et al.", ",");
@@ -64,14 +48,14 @@ function doExport() {
         mem.setlaw(item.reporter);
         mem.setlaw(item.pages);
         memdate.setlaw(item.court,",");
-        const date = Zotero.Utilities.strToDate(item.date);
-        const dateS = (date.year) ? date.year : item.date;
+        let date = Zotero.Utilities.strToDate(item.date);
+        let dateS = (date.year) ? date.year : item.date;
         memdate.set(dateS,"","no date");
         Zotero.write(" " + mem.get() + " " + memdate.get() + " | | |");
         if (Zotero.getHiddenPref("ODFScan.useZoteroSelect")) {
             Zotero.write("zotero://select/items/" + library_id + "_" + item.key + "}");
         } else {
-            const m = item.uri.match(/http:\/\/zotero\.org\/(users|groups)\/([^/]+)\/items\/(.+)/);
+            let m = item.uri.match(/http:\/\/zotero\.org\/(users|groups)\/([^\/]+)\/items\/(.+)/);
             let prefix;
             let lib;
             let key;
